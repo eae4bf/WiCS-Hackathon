f0c20731059da733ba0c935f83f3534e81c31fdd
Jacob Fullerton <fullertonj44@gmail.com>
2019-04-06 10:31:55 -0400

    add some pages and login

--- assignments/views.py
+++ assignments/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.views import generic
+
+from assignments.models import Student
+
+
+class IndexView(generic.ListView):
+    template_name = 'assignments/index.html'
+    model = Student
+    context_object_name = 'students'
+    paginate_by = 5
+
 
 # Create your views here.
