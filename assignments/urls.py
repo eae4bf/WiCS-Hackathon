d1bf40bb96c2db6d79350c83d32ccfd087a475e7
Eliza Eddy <elizaaeddy@gmail.com>
2019-04-06 09:44:11 -0400

    add urls

--- assignments/urls.py
+++ assignments/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-
+from django.conf.urls import url
+from django.contrib.auth import views as auth_views
 from . import views
 
 app_name = 'skillMatch'
 
 urlpatterns = [
+    url(r'^login/$', auth_views.LoginView.as_view(template_name='pages/login.html'), name='login'),
 ]
