970b6c8a01749202137d00ac63c30c2f711e8608
Jacob Fullerton <fullertonj44@gmail.com>
2019-04-06 12:47:47 -0400

    Forms to add assignments

--- assignments/urls.py
+++ assignments/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
     url(r'^login/$', auth_views.LoginView.as_view(template_name='pages/login.html'), name='login'),
     path('todo/', views.todoView.as_view(), name='todo'),
+    path('createAssignment/', views.AssignmentCreateView.as_view(), name='create_assignment'),
 ]
